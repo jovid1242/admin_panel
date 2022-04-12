a65b35144659c9327973940df34aa21c30592a2f
thenavik <iamdigitalist@gmail.com>
2022-04-12 17:47:08 +0300

    Header

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,135 @@
+import React from "react";
+import "../../styles/sidebar/sidebar.scss";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__wrapper">
+        <div className="menu">
+          <ul className="menu__categories">
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+            <li className="menu_categories__list">
+              <a href="#">
+                <div className="">
+                  <div className="icon"></div>
+                  <span>Dashboard</span>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Sidebar from "../components/sidebar/Sidebar";
 import Header from "../components/header/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Sidebar />
     </>
   );
 }
