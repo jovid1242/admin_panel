eeb8194c9140ad75824992f054ef11675dfbfd09
jovid1242 <jovid1242@gmail.com>
2022-04-13 18:42:10 +0500

    fix router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Routes from "./routes/Routes";
+import Routes from "./Router";
 
 function App() {
     return (
--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../pages/Home";
+import Home from "./pages/Home";
 
-export default function AdminRoutes() {
+export default function AdminRouter() {
     return (
         <>
             <Routes>
