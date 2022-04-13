c5f3ea0f2e0d9acbbc2722d16ccc2dd6d4bf03ad
jovid1242 <jovid1242@gmail.com>
2022-04-13 18:15:40 +0500

    fixed sidebar menu list

--- src/components/sidebar/submenu/SubMenu.js
+++ src/components/sidebar/submenu/SubMenu.js
@@ -1,20 +1,24 @@
 import React from "react";
-
+import { BsDot } from "react-icons/bs";
 import "../../../styles/submenu/submenu.scss";
 
 export default function SubMenu({ visibility }) {
-  return (
-    <>
-      <div className={visibility ? "submenu d-none" : "submenu"}>
-        <ul className="submenu__list ">
-          <li>
-            <a href="#">* Sale</a>
-          </li>
-          <li>
-            <a href="#">* Sale</a>
-          </li>
-        </ul>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={visibility ? "submenu d-none" : "submenu"}>
+                <ul className="submenu__list ">
+                    <li>
+                        <a href="#">
+                            <BsDot /> Sale
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <BsDot /> Sale
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
 }
