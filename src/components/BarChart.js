cfd8459d16b3587d8c49b565034db79030f29090
thenavik <iamdigitalist@gmail.com>
2022-04-14 01:14:50 +0300

    Axis chart bar graffic, sales blok, graffic renevue block

---
+++ src/components/BarChart.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import Chart from "react-apexcharts";
+
+function BarChart() {
+    const [state, setState] = useState({
+        series: [
+            {
+                name: "series1",
+                data: [31, 40, 28, 51, 42, 109, 100]
+            },
+            {
+                name: "series2",
+                data: [11, 32, 45, 32, 34, 52, 41]
+            }
+        ],
+        options: {
+            chart: {
+                height: 350,
+                type: "area",
+                toolbar: {
+                    show: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            // stroke: {
+            //     curve: "smooth"
+            // },,
+            grid: {
+                show: true,
+                borderType: "dashed",
+                yaxis: {
+                    lines: {
+                        show: false
+                    }
+                },
+
+                xaxis: {
+                    lines: {
+                        show: false,
+                        type: "dotted"
+                    },
+                    crosshairs: {
+                        show: true
+                    },
+                    tickPlacement: "between"
+                }
+            },
+            tooltip: {
+                x: {
+                    format: "dd/MM/yy HH:mm"
+                }
+            },
+            legend: {
+                show: true,
+                showForSingleSeries: false,
+                showForNullSeries: true,
+                showForZeroSeries: true,
+                position: "top",
+                horizontalAlign: "right"
+            },
+            fill: {
+                opacity: 0.05,
+                type: "gradient",
+                gradient: {
+                    shade: "dark",
+                    type: "vertical",
+                    shadeIntensity: 0.5,
+                    gradientToColors: undefined,
+                    inverseColors: true,
+                    opacityFrom: 0.1,
+                    opacityTo: 0.5,
+                    stops: [0, 50, 100],
+                    colorStops: []
+                }
+            },
+            stroke: {
+                show: true,
+                curve: "smooth",
+                lineCap: "butt",
+                colors: ["red", "blue"],
+                width: 2,
+                dashArray: 0
+            }
+        }
+    });
+
+    return (
+        <div>
+            <Chart
+                options={state.options}
+                series={state.series}
+                type="area"
+                height={365}
+                width={828}
+            />
+        </div>
+    );
+}
+
+export default BarChart;
--- src/components/dashboard/sales.js
+++ src/components/dashboard/sales.js
@@ -1,10 +1,36 @@
 import React from "react";
 import "../../styles/dashboard/sales.scss";
+import { HiDotsHorizontal } from "react-icons/hi";
+import BarChart from "../BarChart";
 
 export default function Sales() {
     return (
         <div>
-            <p>Here are the sales things</p>
+            <div className="row">
+                <div className="renevue">
+                    <div className="renevueHeadline">
+                        <p>Renevue</p>
+                        <div className="renevueDropdown">
+                            <a href="#">
+                                <HiDotsHorizontal />
+                            </a>
+                            <div className="dropdownMenu">
+                                <a className="dropdownItem">Weekly</a>
+                                <a className="dropdownItem">Monthly</a>
+                                <a className="dropdownItem">Yearly</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="barChart">
+                        <BarChart />
+                    </div>
+                </div>
+                <div className="byCategory">
+                    <div className="headline">
+                        <h5>Sales by Category</h5>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
