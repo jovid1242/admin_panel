d02ad837724d1044c99104a81c806aeb05495eba
thenavik <iamdigitalist@gmail.com>
2022-04-13 19:56:39 +0300

    dashboard

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
 import Home from "./pages/Home";
+import Header from "./components/header/Header";
+import Sidebar from "./components/sidebar/Sidebar";
+import Main from "./components/main/Main";
 
 export default function AdminRouter() {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
+            <Header />
+            <Sidebar />
+            <Main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+            </Main>
         </>
     );
 }
--- src/components/dashboard/sales.js
+++ src/components/dashboard/sales.js
@@ -1,2 +1,10 @@
 import React from "react";
 import "../../styles/dashboard/sales.scss";
+
+export default function Sales() {
+    return (
+        <div>
+            <p>Here are the sales things</p>
+        </div>
+    );
+}
