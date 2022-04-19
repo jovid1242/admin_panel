b5fb3248460f25f72663b4fb9037d399e1284c42
thenavik <iamdigitalist@gmail.com>
2022-04-19 14:15:27 +0300

    alerts page done

---
+++ src/core/alerts/XAlert.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'styles/core/alerts/xalert.scss'
+
+export default function XAlert({ type }) {
+    return (
+        <div className={'alert ' + 'alert-' + type}>
+            Alert! Lorem Ipsum is simply dummy text of the printing.
+            <span className="closebtn" onclick="">
+                &times;
+            </span>
+        </div>
+    )
+}
